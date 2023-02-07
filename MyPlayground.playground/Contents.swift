d2cd6d83508e5d0f40d15e1124ae988066d0b4ce
KirillBelui <sergantbelui@gmail.com>
2023-02-07 22:59:20 +0200

    replace print(firstName) below print(greeting, secondName)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,4 +3,5 @@ import UIKit
 var greeting = "Hello, playground"
 var firstNumber = 10
 var secondNumber = 13
-print(greeting, firstNumber, secondNumber)
+print(firstNumber)
+print(greeting, secondNumber)
